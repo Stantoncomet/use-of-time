db5e86352323fc049f7356476fbca347f976ef3c
Stantoncomet <olivercraftmoore@gmail.com>
2026-07-09 01:19:20 -0700

    67 pack wm

---
+++ whatmodpack/67pack/why.js
@@ -0,0 +1,41 @@
+//i hsouldnt need this but extneions were injecting weird code and breaking scripts in the index
+
+document.addEventListener('DOMContentLoaded', e => {
+    document.getElementById('ml-input').value = `
+
+<html><body><ul> 	<li><a href="https://modrinth.com/mod/zV5r3pPn">3d-Skin-Layers</a></li> 	<li><a href="https://modrinth.com/mod/laX5CckD">Accelerated Decay</a></li> 	<li><a href="https://modrinth.com/mod/EsAfCjCV">AppleSkin</a></li> 	<li><a href="https://modrinth.com/mod/OfKzpbRU">BaguetteLib</a></li> 	<li><a href="https://modrinth.com/mod/MBAkmtvl">Balm</a></li> 	<li><a href="https://modrinth.com/mod/HXF82T3G">Biomes O' Plenty</a></li> 	<li><a href="https://modrinth.com/mod/Wb5oqrBJ">Chat Heads</a></li> 	<li><a href="https://modrinth.com/mod/4KWv7wbN">Chisel Reborn</a></li> 	<li><a href="https://modrinth.com/mod/9s6osm5g">Cloth Config v26.1 API</a></li> 	<li><a href="https://modrinth.com/mod/x18RjbsF">Colossal Reactors</a></li> 	<li><a href="https://modrinth.com/mod/SaCpeal4">Comforts</a></li> 	<li><a href="https://modrinth.com/mod/oFXrCkDI">CommonCapabilities</a></li> 	<li><a href="https://modrinth.com/mod/xv94TkTM">Controlling</a></li> 	<li><a href="https://modrinth.com/mod/WrpuIfhw">Corpse</a></li> 	<li><a href="https://modrinth.com/mod/pJGcKPh1">Corpse Curios Compatibility</a></li> 	<li><a href="https://modrinth.com/mod/cl223EMc">Cristel Lib</a></li> 	<li><a href="https://www.curseforge.com/projects/415438">Croptopia</a></li> 	<li><a href="https://modrinth.com/mod/oEhQIkOs">Cryonic Config</a></li> 	<li><a href="https://modrinth.com/mod/Rw1NrDzF">Cucumber Library</a></li> 	<li><a href="https://modrinth.com/mod/vvuO3ImH">Curios API</a></li> 	<li><a href="https://modrinth.com/mod/Z9DM0LJ4">Cyclops Core</a></li> 	<li><a href="https://modrinth.com/mod/8Dbnvm77">Dungeons and Taverns Nether Fortress Overhaul</a></li> 	<li><a href="https://modrinth.com/mod/LQ3K71Q1">Dynamic FPS</a></li> 	<li><a href="https://modrinth.com/mod/a8Jk9kpK">EdivadLib</a></li> 	<li><a href="https://modrinth.com/mod/CLOsbQfZ">EnchantOnce</a></li> 	<li><a href="https://modrinth.com/mod/UVtY3ZAC">EnchantmentDescriptions</a></li> 	<li><a href="https://modrinth.com/mod/ZJTGwAND">End Remastered</a></li> 	<li><a href="https://modrinth.com/mod/NNAgCjsB">EntityCulling</a></li> 	<li><a href="https://www.curseforge.com/projects/885449">EpheroLib</a></li> 	<li><a href="https://www.curseforge.com/projects/404465">FTB Library</a></li> 	<li><a href="https://www.curseforge.com/projects/386134">FTB Ultimine</a></li> 	<li><a href="https://modrinth.com/mod/uXXizFIs">Ferrite Core</a></li> 	<li><a href="https://modrinth.com/mod/p19vrgc2">Fusion</a></li> 	<li><a href="https://modrinth.com/mod/s3dmwKy5">GlitchCore</a></li> 	<li><a href="https://modrinth.com/mod/5ZwdcRci">ImmediatelyFast</a></li> 	<li><a href="https://modrinth.com/mod/yYzdQHJI">IntegratedDynamics</a></li> 	<li><a href="https://modrinth.com/mod/Etqy1Omb">IntegratedTunnels</a></li> 	<li><a href="https://modrinth.com/mod/YL57xq9U">Iris</a></li> 	<li><a href="https://modrinth.com/mod/yPlaLxD1">Iron Furnaces</a></li> 	<li><a href="https://modrinth.com/mod/y16HgCOF">Iskandert Library</a></li> 	<li><a href="https://modrinth.com/mod/nvQzSEkH">Jade</a></li> 	<li><a href="https://modrinth.com/mod/DosZdmJF">Jake's Build Tools</a></li> 	<li><a href="https://modrinth.com/mod/u6dRKJwZ">Just Enough Items</a></li> 	<li><a href="https://modrinth.com/mod/umyGl7zF">KubeJS</a></li> 	<li><a href="https://modrinth.com/mod/yBW8D80W">LambDynamicLights</a></li> 	<li><a href="https://modrinth.com/mod/gvQqBUqZ">Lithium</a></li> 	<li><a href="https://modrinth.com/mod/EltpO5cN">Lootr</a></li> 	<li><a href="https://modrinth.com/mod/bq6nqeOx">Medieval Buildings [The End Edition]</a></li> 	<li><a href="https://modrinth.com/mod/nmDcB62a">ModernFix</a></li> 	<li><a href="https://modrinth.com/mod/1oUDhxuy">Moog's Structure Lib</a></li> 	<li><a href="https://modrinth.com/mod/r4PuRGfV">MoogsEndStructures</a></li> 	<li><a href="https://modrinth.com/mod/fPetb5Kh">Nature's Compass</a></li> 	<li><a href="https://modrinth.com/mod/MmWjgmtL">Nether Merchant</a></li> 	<li><a href="https://modrinth.com/mod/iRmWy6ga">Pipez</a></li> 	<li><a href="https://modrinth.com/mod/3uT4J18I">Polymorph</a></li> 	<li><a href="https://modrinth.com/mod/aaRl8GiW">PrickleMC</a></li> 	<li><a href="https://www.curseforge.com/projects/1597601">ProjectEE v1.2.2</a></li> 	<li><a href="https://modrinth.com/mod/KDvYkUg3">Refined Storage</a></li> 	<li><a href="https://modrinth.com/mod/VzR5wiLo">Refined Storage - JEI Integration</a></li> 	<li><a href="https://modrinth.com/mod/gnwGOmBf">Refined Storage - Quartz Arsenal</a></li> 	<li><a href="https://modrinth.com/mod/sk9knFPE">Rhino</a></li> 	<li><a href="https://modrinth.com/mod/fuuu3xnx">Searchables</a></li> 	<li><a href="https://modrinth.com/mod/bi4iCmsw">Shogi</a></li> 	<li><a href="https://modrinth.com/mod/73mSSnCf">Silent Gear</a></li> 	<li><a href="https://modrinth.com/mod/BQhuHQo4">Silent Lib</a></li> 	<li><a href="https://modrinth.com/mod/Y036wakZ">Silent's Gems</a></li> 	<li><a href="https://modrinth.com/mod/EUV7foM2">Skniro's Nether and End Ores</a></li> 	<li><a href="https://modrinth.com/mod/AANobbMI">Sodium</a></li> 	<li><a href="https://modrinth.com/mod/PtjYWJkn">Sodium Extra</a></li> 	<li><a href="https://modrinth.com/mod/jfd8j2j2">SolarGeneration</a></li> 	<li><a href="https://modrinth.com/mod/TyCTlI4b">Sophisticated Backpacks</a></li> 	<li><a href="https://modrinth.com/mod/nmoqTijg">Sophisticated Core</a></li> 	<li><a href="https://modrinth.com/mod/Rw1Ylmn4">Stardew Fishing</a></li> 	<li><a href="https://modrinth.com/mod/kkmrDlKT">TerraBlender</a></li> 	<li><a href="https://modrinth.com/mod/JD2NSu5O">The Aether II</a></li> 	<li><a href="https://modrinth.com/mod/DjLobEOy">Towns and Towers</a></li> 	<li><a href="https://modrinth.com/mod/LOpKHB2A">Waystones</a></li> 	<li><a href="https://modrinth.com/mod/AnpW69o3">XP Tome</a></li> 	<li><a href="https://modrinth.com/mod/1bokaNcj">Xaero's Minimap</a></li> 	<li><a href="https://modrinth.com/mod/NcUtCpym">Xaero's World Map</a></li> 	<li><a href="https://modrinth.com/mod/Ua7DFN59">YUNG's API</a></li> 	<li><a href="https://modrinth.com/mod/o1C1Dkj5">YUNG's Better Dungeons</a></li> 	<li><a href="https://modrinth.com/mod/z9Ve58Ih">YUNG's Better Jungle Temples</a></li> 	<li><a href="https://modrinth.com/mod/HjmxVlSr">YUNG's Better Mineshafts</a></li> </ul></body></html>
+
+    `
+    load();
+
+    serverStatus();
+})
+
+async function serverStatus() {
+    let res = await fetch('https://api.mcstatus.io/v2/status/java/stantoncomet.gleeze.com:24454');
+    let data = await res.json();
+    console.log(data)
+
+    let status = ``;
+
+    if (data.online) {
+        // set status to player count / max players. also if the max is absurdly long, 
+        // it just uses the 2 most significant figures
+        status = `${data.players.online}/${data.players.max.toString().slice(0,2)}`;
+        
+        let playerlist = data.players.list;
+
+        // player list string
+        playerlist = playerlist.map(p => p.name_clean).toString();
+        // replace , with newlines
+        playerlist = playerlist.replaceAll(',', '\n');
+
+        playerlist = `Player List:\n${playerlist}`;
+
+        document.getElementById('server-status').setAttribute('title', playerlist);
+    } else {
+        status = "Offline";
+    }
+
+    document.getElementById('server-status').innerText = status;
+}
